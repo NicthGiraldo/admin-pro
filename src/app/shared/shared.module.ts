e0ff3fc9c89aa5a19f99568003f73a3b2563923f
Nicth Giraldo <nicth.giraldo@gmail.com>
2018-05-17 16:45:47 -0500

    correccion de error shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
     declarations: [
         HeaderComponent,
         SidebarComponent,
-        BreadcrumbsComponent
+        BreadcrumbsComponent,
+        NopagefoundComponent
     ],
     exports: [
         HeaderComponent,
         SidebarComponent,
-        BreadcrumbsComponent
+        BreadcrumbsComponent,
+        NopagefoundComponent
     ]
 })
 export class SharedModule { }
